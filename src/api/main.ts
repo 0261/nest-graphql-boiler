41c67093b4370077172486d3280222d7baa5386d
0261 <bada013@naver.com>
2021-03-25 02:07:01 +0900

    build(api.main): add cors and change port

--- src/api/main.ts
+++ src/api/main.ts
@@ -3,7 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  await app.listen(9821);
 }
 void bootstrap();
