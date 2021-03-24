6556969750e63f788ffb6c873d960530a70554c4
0261 <bada013@naver.com>
2021-03-25 02:07:59 +0900

    feat(graphql.guard): add authentication guard for graphql

---
+++ src/graphql/guard/authentication.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ForbiddenError } from 'apollo-server-express';
+import { Request } from 'express';
+
+import { AuthenticationService } from '~core/authentication';
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+  public constructor(private readonly auth: AuthenticationService) { }
+
+  public canActivate(context: ExecutionContext): boolean {
+    const gqlContext: { user?: AuthenticationService.User; req: Request; } = GqlExecutionContext.create(context).getContext();
+    const request: Request = gqlContext.req;
+    const token: undefined | string = request.header('Authentication');
+
+    if (token === '' || token === 'null' || token === 'undefined') {
+      throw new ForbiddenError('Forbidden');
+    }
+
+    if (token === undefined) {
+      throw new ForbiddenError('Forbidden');
+    }
+    else {
+      void this.auth.authenticateByToken(token);
+    }
+
+    return true;
+  }
+}
